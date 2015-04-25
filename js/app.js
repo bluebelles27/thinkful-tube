8e0cc7fd84de01a1f5bda659e01f999f80ce0382
amalthea5 <erin.knapp@gmail.com>
2015-04-25 19:19:08 -0400

    Took out comments

--- js/app.js
+++ js/app.js
@@ -1,15 +1,7 @@
-// Helper function to display JavaScript value on HTML page.
-//function showResponse(response) {
-  //  var responseString = JSON.stringify(response, '', 2);
-    //document.getElementById('response').innerHTML += responseString;
-//}
-
-// Called automatically when JavaScript client library is loaded.
 function onClientLoad() {
     gapi.client.load('youtube', 'v3', onYouTubeApiLoad);
 }
 
-// Called automatically when YouTube API interface is loaded 
 function onYouTubeApiLoad() {
     gapi.client.setApiKey('AIzaSyCZhJvq9XRp__3rvl0RrJdUFE3okUx6_sU');
 
@@ -17,15 +9,12 @@ function onYouTubeApiLoad() {
 }
 
 function search() {
-    // Use the JavaScript client library to create a search.list() API call.
     var q = $('#query').val();
     var request = gapi.client.youtube.search.list({
         part: 'snippet',
         q: q
     });
     
-    // Send the request to the API server,
-    // and invoke onSearchRepsonse() with the response.
 request.execute(function(response) {
     var str = JSON.stringify(response.result);
     $('#search-container').html('<pre>' + str + '</pre>');
