e986ddc39039c10333016a69dec1281564eb039d
amalthea5 <erin.knapp@gmail.com>
2015-04-25 23:10:19 -0400

    More fixes

--- js/app.js
+++ js/app.js
@@ -1,21 +1,19 @@
 
 $(document).ready(function() {
-  $('#search-button').click(function(){
-    function search() {
+  $('#search-button').click(function search(){
     var q = $('#query').val();
-    };
+   });
 
-    var request = gapi.client.youtube.search.list({
-        key: "AIzaSyCZhJvq9XRp__3rvl0RrJdUFE3okUx6_sU",
-        q: q,
-        part: 'snippet'
-      });
+   var request = gapi.client.youtube.search.list({
+       key: "AIzaSyCZhJvq9XRp__3rvl0RrJdUFE3okUx6_sU",
+       q: q,
+       part: 'snippet'
+    });
       //gapi.client.load('youtube', 'v3', function() {}
 
-      request.execute(function(response) {
-        var str = JSON.stringify(response.result);
-         $('#search-container').html('<pre>' + str + '</pre>');
+    request.execute(function(response) {
+    	var str = JSON.stringify(response.result);
+        $('#search-container').html('<pre>' + str + '</pre>');
         console.log(str);
-      });
-  };
+    });
 });
