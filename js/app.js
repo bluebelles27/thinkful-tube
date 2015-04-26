f753ee50d7c635dd7020c5256627412456184ad3
amalthea5 <erin.knapp@gmail.com>
2015-04-25 20:04:46 -0400

    Used Google Tutorial script

--- js/app.js
+++ js/app.js
@@ -1,21 +1,12 @@
-function onClientLoad() {
-    gapi.client.load('youtube', 'v3', onYouTubeApiLoad);
-}
-
-function onYouTubeApiLoad() {
-    gapi.client.setApiKey('AIzaSyCZhJvq9XRp__3rvl0RrJdUFE3okUx6_sU');
-
-    search();
-}
-
+// Search for a specified string.
 function search() {
-    var q = $('#query').val();
-    var request = gapi.client.youtube.search.list({
-        part: 'snippet',
-        q: q
-    });
-    
-request.execute(function(response) {
+  var q = $('#query').val();
+  var request = gapi.client.youtube.search.list({
+    q: q,
+    part: 'snippet'
+  });
+
+  request.execute(function(response) {
     var str = JSON.stringify(response.result);
     $('#search-container').html('<pre>' + str + '</pre>');
   });
