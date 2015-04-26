5ee5782176ee230b3fc2a18a0c23f81149b904d4
amalthea5 <erin.knapp@gmail.com>
2015-04-25 21:56:20 -0400

    formatting

--- js/app.js
+++ js/app.js
@@ -1,18 +1,18 @@
 
 $(document).ready(function() {  
 
-gapi.client.load('youtube', 'v3', function() {
-  var q = $('#query').val();
-  var request = gapi.client.youtube.search.list({
-    key: "AIzaSyCZhJvq9XRp__3rvl0RrJdUFE3okUx6_sU",
-    q: q,
-    part: 'snippet'
-  });
+	gapi.client.load('youtube', 'v3', function() {
+  	var q = $('#query').val();
+  	var request = gapi.client.youtube.search.list({
+    	key: "AIzaSyCZhJvq9XRp__3rvl0RrJdUFE3okUx6_sU",
+    	q: q,
+    	part: 'snippet'
+  	});
 
-  request.execute(function(response) {
-    var str = JSON.stringify(response.result);
-     $('#search-container').html('<pre>' + str + '</pre>');
-    console.log(str);
-  });
-});
+  	request.execute(function(response) {
+    	var str = JSON.stringify(response.result);
+     	$('#search-container').html('<pre>' + str + '</pre>');
+    	console.log(str);
+  	});
+	});
 });
