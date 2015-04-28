10907b1ba660aa1cbe7ee5bc3bee4fa05041f73d
amalthea5 <erin.knapp@gmail.com>
2015-04-27 21:40:09 -0400

    More JS editing

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
 function init() {
   gapi.client.setApiKey('AIzaSyCZhJvq9XRp__3rvl0RrJdUFE3okUx6_sU');
-  gapi.client.load('youtube', 'v3').then(makeRequest);
+  gapi.client.load('youtube', 'v3');
 }
 
 $(document).ready(function() {
   $('#search-button').click(function search(){
     var q = $('#query').val();
-   });
+   
 
    var request = gapi.client.youtube.search.list({
        q: q,
@@ -18,5 +18,6 @@ $(document).ready(function() {
     	var str = JSON.stringify(response.result);
         $('#search-container').html('<pre>' + str + '</pre>');
         console.log(str);
+    });
     });
 });
