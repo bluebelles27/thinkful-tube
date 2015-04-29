70e11b54fd1c272666f365ab8a8516d5ea35bb8a
amalthea5 <erin.knapp@gmail.com>
2015-04-29 12:04:18 -0400

    display function

--- js/app.js
+++ js/app.js
@@ -21,7 +21,7 @@ $(document).ready(function() {
     });
   	function displaySearchResults(videos) {
 		var html = "";
-		$.each(videos, function (index, video) {
+		$.each(videos, function (index, items) {
 			console.log(videos);
 			html = html + "<li><p>" + items.snippet.title +
 				"</p><img src='" +  items.snippet.thumbnails.high.url + "'/></li>" ;
